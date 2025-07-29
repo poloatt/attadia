082d9a09fe002cbfe0ecc99094efc1a5d6a61992
poloatt <polo@poloatt.com>
2025-07-28 21:03:22 -0300

    probemos

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -47,7 +47,7 @@ export default defineConfig(({ mode }) => {
       force: true
     },
     build: {
-      outDir: 'dist',
+      outDir: '.',
       sourcemap: !isProd,
       minify: isProd,
       rollupOptions: {
